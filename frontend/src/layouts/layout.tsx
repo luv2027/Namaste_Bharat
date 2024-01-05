7f4863af2ad6ca9e38df731e6eb5290ed3f5a21e
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-05 22:48:05 +0530

    added footer

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,11 +1,20 @@
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import  Header  from "../components/header";
 
-const Layout = () => {
+interface Props{
+  children: React.ReactNode;
+}
+
+const Layout = ({children}: Props) => {
   return (
     <div className = "flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <div className = "container mx-auto py-10 flex-1">
+        {children}
+      </div>
+      <Footer />
     </div>
   )
 }
