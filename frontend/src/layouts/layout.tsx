130835e0c9d0bc3c53d597ef3ed1f5b83e41f568
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-05 22:27:04 +0530

    till hero of the page

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,9 +1,11 @@
+import Hero from "../components/Hero";
 import  Header  from "../components/header";
 
 const Layout = () => {
   return (
     <div className = "flex flex-col min-h-screen">
       <Header />
+      <Hero />
     </div>
   )
 }
