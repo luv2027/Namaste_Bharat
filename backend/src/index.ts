6e6bae28080c9d393a01cdba6ea67d32dbac8595
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-06 16:58:32 +0530

    done till signup Api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express  from "express";
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,10 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
-app.get("/api/test", async (req, res) => {
-  res.json({message: "Hello from express endpoint!"});
-})
+app.use("/api/users", userRoutes)
 
 app.listen(7000, () => {
   console.log("Server is listening on port 7000");
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,15 +1,27 @@
-import express from "express";
+import express, {Request, Response}  from "express";
 import User from "../models/user"
 import jwt from "jsonwebtoken";
+import { check } from "express-validator";
+import { validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", async(req, res) => {
+// /api/users/register
+router.post("/register", [
+  check("firstName", "First Name is required").isString(), 
+  check("lastName", "Last Name is required").isString(),
+  check("email", "Email is required").isEmail(),
+  check("password", "Password with 6 or more characters is required").isLength({min: 6})
+], async(req : Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({message: errors.array()});
+  }
   try{
     let user = await User.findOne({
       email: req.body.email,
-    })
-
+    });
 
     if(user){
       return res.status(400).json({
@@ -34,7 +46,7 @@ router.post("/register", async(req, res) => {
   }
   catch(error){
     console.log(error);
-    res.status(500).send({message: "Something went wrong"});
+    res.status(400).send({message: "Something went wrong"});
   }
 })
 
