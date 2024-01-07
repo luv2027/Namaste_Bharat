56db70f47a79b7f8d872aa23ed29cb644cce66f2
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-07 18:41:17 +0530

    navigation back to home page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import {useForm} from 'react-hook-form';
 import {useMutation} from 'react-query';
 import * as apiClient from "../api-client";
 import {useAppContext} from "../contexts/AppContext";
+import { useNavigate } from 'react-router-dom';
 
 export type RegisterFormData = {
   firstName: string;
@@ -13,6 +14,8 @@ export type RegisterFormData = {
 
 const Register = () => {
 
+  const navigate = useNavigate();
+
   const {showToast} = useAppContext();
 
   const {register, watch , handleSubmit, formState: { errors }} = useForm<RegisterFormData>();
@@ -22,6 +25,7 @@ const Register = () => {
       showToast({
         message: "Registration Success!", type:"SUCCESS"
       });
+      navigate("/");
     },
     onError: (error: Error) => {
       showToast({
