0d7bfc0600bbea2a7cbc72c05085573a08a95890
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-16 01:18:19 +0530

    before deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -16,6 +17,9 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
+
+//taken static files from frontend for depoloyment made the frontend and backend in same folder for depoloy and no need to run on differnt ports
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
