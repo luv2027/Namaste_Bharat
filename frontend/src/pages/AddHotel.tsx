afeb1b039fee633c7aabbb05dd1db1800372c238
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-01-22 22:17:13 +0530

    done adding hotels to mongodb

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,8 +1,26 @@
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import {useMutation} from "react-query";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
 
+const AddHotel = () =>{
 
-const AddHotel = () => {
-  return (<ManageHotelForm />)
-}
+  const {showToast} = useAppContext();
+
+  const {mutate, isLoading} = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({message: "Hotel Saved!", type: "SUCCESS"});
+    },
+    onError: () => {
+      showToast({message: "Error in saving hotel", type: "ERROR"})
+    }
+  });
+
+  const handleSave = (hotelFormData : FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (<ManageHotelForm onSave = {handleSave} isLoading={isLoading} />);
+};
 
 export default AddHotel;
