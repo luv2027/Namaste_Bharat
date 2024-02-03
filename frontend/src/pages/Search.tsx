038de5df38427799255227392612c49449973288
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-02-04 02:59:48 +0530

    added facilites filter

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -2,6 +2,7 @@ import { useSearchContext } from "../contexts/SearchContext";
 import {useQuery} from "react-query";
 import * as apiClient from "../api-client";
 import { useState } from "react";
+import SearchResultsCard from "../components/SearchResultsCard";
 
 const Search = () => {
   const search = useSearchContext();
@@ -17,10 +18,36 @@ const Search = () => {
     page: page.toString(),
   }
   
-  const {data: hotelData} = useQuery(["searchHotels", searchParams], () => {
-    apiClient.searchHotels(searchParams)
-  })
-  return <>Search Page</>;
+  const {data: hotelData} = useQuery(["searchHotels", searchParams], () => 
+    apiClient.searchHotels(searchParams))
+
+  return (
+    <div className = "grid grid-cols-1 1g:grid-cols-[250px_1fr] gap-5">
+      <div className = "rounded-1g border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className = "text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+          </h3>
+          {}
+        </div>
+      </div>
+      <div className = "flex flex-col gap-5">
+        <div className = "flex justify-between itmes-center">
+          <span className = "text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? `in ${search.destination}` : ""}
+          </span>
+          {}
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultsCard hotel={hotel}/>
+        ))}
+        <div>
+          <Pagination page={hotelData?.pagination.page || 1}/>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search;
