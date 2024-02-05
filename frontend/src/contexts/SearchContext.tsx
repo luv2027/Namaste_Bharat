7ae5331a394684b994fd29844cf655cb104f932a
Luv Patel <luvpatel2790.svmr@gmail.com>
2024-02-05 23:56:12 +0530

    done till booking.tsx

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 //will take all the search values and save it in the context. Will use this context in the search page to display the search results
 
 type SearchContext = {
@@ -19,12 +19,27 @@ type SearchContextProviderProps = {
 };
 
 export const SearchContextProvider  = ({children}: SearchContextProviderProps) => {
-  const [destination, setDestination] = React.useState<string>("");
-  const [checkIn, setCheckIn] = React.useState<Date>(new Date());
-  const [checkOut, setCheckOut] = React.useState<Date>(new Date());
-  const [adultCount, setAdultCount] = React.useState<number>(1);
-  const [childCount, setChildCount] = React.useState<number>(0);
-  const [hotelId, setHotelId] = React.useState<string>("");
+  const [destination, setDestination] = useState<string>(
+    () => sessionStorage.getItem("destination") || ""
+  );
+  const [checkIn, setCheckIn] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem("checkIn") || new Date().toISOString())
+  );
+  const [checkOut, setCheckOut] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem("checkOut") || new Date().toISOString())
+  );
+  const [adultCount, setAdultCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem("adultCount") || "1")
+  );
+  const [childCount, setChildCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem("childCount") || "1")
+  );
+  const [hotelId, setHotelId] = useState<string>(
+    () => sessionStorage.getItem("hotelID") || ""
+  );
+
 
   const saveSearchValues = (destination: string, checkIn: Date, checkOut: Date, adultCount: number, childCount: number, hotelId? : string) => {
     setDestination(destination);
@@ -34,6 +49,16 @@ export const SearchContextProvider  = ({children}: SearchContextProviderProps) =
     setChildCount(childCount);
     if(hotelId){
       setHotelId(hotelId);
+    }
+
+    sessionStorage.setItem("destination", destination);
+    sessionStorage.setItem("checkIn", checkIn.toISOString());
+    sessionStorage.setItem("checkOut", checkOut.toISOString());
+    sessionStorage.setItem("adultCount", adultCount.toString());
+    sessionStorage.setItem("childCount", childCount.toString());
+
+    if (hotelId) {
+      sessionStorage.setItem("hotelId", hotelId);
     }
   };
 
